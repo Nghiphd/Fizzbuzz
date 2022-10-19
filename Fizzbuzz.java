13dbef32aadaf0fe17a71c49c8f088c9c4c8c035
Nghi Phan <nghi.phan@mrjaffesclass.com>
2022-10-19 11:45:19 -0700

    close

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -1,3 +1,13 @@
 public class Fizzbuzz {
-
+    public static void main(String args[]) {
+        fizzbuzz();
+    }
+    public int fizzbuzz(int fizz, int buzz) {
+        int i = 0;
+        buzz = 0;
+        for(fizz = 1; fizz*3 <= 100; fizz = fizz * 3) {
+            return fizz;
+        }
+        return i;
+    }
 }
