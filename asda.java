f46e779f206bc12d7dd34eea211c7ec7aed32810
Nghi Phan <nghi.phan@mrjaffesclass.com>
2022-10-28 11:44:31 -0700

    extra

--- asda.java
+++ asda.java
@@ -1,10 +1,11 @@
 public class asda {
     public static void main(String args[]) {
         int i;
-        for(i = 1; i < 101; i++) {
-            if (i*3/3 == i) {
+        for(i = 1; i < 101; i= i + 1) {
+            if (i/3 * 3 == i) {
                 System.out.println("Fizz");
             }
+            System.out.println(i);
         }
     }
 }
