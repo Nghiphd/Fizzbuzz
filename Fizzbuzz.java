0e9e3cc770dba0e1bea7907279fd06ddd718e6f4
Nghi Phan <nghi.phan@mrjaffesclass.com>
2022-10-28 11:41:35 -0700

    3/4

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -2,13 +2,13 @@ public class Fizzbuzz {
     public static void main(String args[]) {
         int i;
         for(i = 1; i < 101; i++) {
-            if (i % 15 == 0) {
+            if (i/15 * 15 == i) {
                 System.out.println ("FizzBuzz");
             }
-            else if (i % 3 == 0) {
+            else if (i*3/3 == i) {
                 System.out.println("Fizz");
             }
-            else if (i % 5 == 0) {
+            else if (i/5 * 5 == i) {
                 System.out.println("Buzz");
             }
             else {
--- asda.java
+++ asda.java
@@ -1,14 +1,10 @@
 public class asda {
     public static void main(String args[]) {
-        int fizz = 1;
-        asda hw = new asda();
-        hw.fuzz(1, 1);
-    }
-    public void fuzz(int x, int y) {
-        int i = 0;
-        y = 0;
-        for(x = 0; x <= 100; x = (x+1)*3) {
-            System.out.println("fizz");
+        int i;
+        for(i = 1; i < 101; i++) {
+            if (i*3/3 == i) {
+                System.out.println("Fizz");
+            }
         }
     }
 }
